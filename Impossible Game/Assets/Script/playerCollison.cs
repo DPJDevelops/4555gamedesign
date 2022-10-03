d2ad3b487ca2acb6e4356bf8879d6319c06c4417
Ahollow2 <adrianhway@gmail.com>
2022-10-02 21:06:22 -0700

    The Template
    Finally figured it out did it the old school way

---
+++ Impossible Game/Assets/Script/playerCollison.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollison : MonoBehaviour
+{
+    public playerMovement movement;
+
+    
+    
+    public void OnCollisionEnter(UnityEngine.Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "Enemy")
+        {
+            // Two ways to do this first one is commented
+            // movement.enabled = false;
+           // GetComponent<playerMovement>().enabled = false;
+             FindObjectOfType<GameManager>().Endgame();
+          // FindObjectOfType<GameManager>().Restart();
+
+          
+        }
+        // The below code does not work 
+        /*if (collisionInfo.GetComponent<playerMovement>() != null)
+           { collisionInfo.transform.position = collisionInfo.FindObjectOfType<playerMovement>().startPos;}
+           */ 
+
+        
+        
+    }
+}
